644abdef1a48934d4461d2c985e9e00f4b57196b
Nik Todorov <ntodorov@gmail.com>
2024-06-17 18:30:39 -0600

    added unit test and some refactoring

--- src/action.js
+++ src/action.js
@@ -1,67 +1,17 @@
-const { CosmosClient } = require('@azure/cosmos');
 const fs = require('fs');
 const path = require('path');
 const dotenv = require('dotenv');
-
-// Execute a migration script
-async function executeMigration(client, scriptPath) {
-  //load the migration script
-  const script = require(scriptPath);
-
-  const database = client.database(script.databaseName);
-  //connect to the container that the migration script is targeting
-  const container = database.container(script.containerName);
-  //connect to the dedicated for MIGRATIONS container
-  const { container: migrationsContainer } =
-    await database.containers.createIfNotExists({ id: '_migrations' });
-
-  //load the migration history for this container
-  const scriptName = path.basename(scriptPath);
-  const migrationHistory = migrationsContainer.item(
-    script.containerName,
-    undefined
-  );
-  const { resource: migrations } = await migrationHistory.read();
-
-  //check if the script was already executed
-  const executedOn = migrations?.scripts?.find(
-    (script) => script.scriptName === scriptName
-  )?.executedOn;
-
-  //if it was already executed, skip it
-  if (executedOn) {
-    console.log(
-      `Skipping migration: ${scriptName}. It was previously executed on "${executedOn}".`
-    );
-    return;
-  }
-
-  //MAIN WORK HERE: execute the script
-  await script.run(database, container); // Assuming each script has a run method
-
-  //record the execution in the migration history
-  if (migrations)
-    migrations.scripts.push({
-      scriptName,
-      executedOn: new Date().toISOString(),
-    });
-
-  const migrationItem = migrations ?? {
-    id: script.containerName,
-    scripts: [{ scriptName, executedOn: new Date().toISOString() }],
-  };
-  //save the migration history
-  await migrationsContainer.items.upsert(migrationItem);
-}
+const { CosmosClient } = require('@azure/cosmos');
+const { executeMigration } = require('./execute-migration');
 
 async function migrate(options) {
   dotenv.config();
 
-  const endpoint = options.dbURL || process.env.DB_URL;
-  const key = options.dbKey || process.env.DB_KEY;
+  const endpoint = options.dbURL || process.env.DB_URL || '';
+  const key = options.dbKey || process.env.DB_KEY || '';
   const migrationsDir = path.resolve(
     process.cwd(),
-    options.migrationsDir || process.env.MIGRATIONS_DIR
+    options.migrationsDir || process.env.MIGRATIONS_DIR || ''
   );
 
   const errors = [];
---
+++ src/action.test.js
@@ -0,0 +1,96 @@
+jest.mock('fs', () => ({
+  fileExistsSync: jest.fn().mockReturnValue(true),
+  readdirSync: jest.fn().mockReturnValue(['migration1.js', 'migration2.js']),
+}));
+const { fileExistsSync, readdirSync } = require('fs');
+
+jest.mock('@azure/cosmos', () => ({
+  CosmosClient: jest.fn().mockImplementation(() => ({
+    database: jest.fn().mockReturnThis(),
+    container: jest.fn().mockReturnThis(),
+    containers: {
+      createIfNotExists: jest.fn().mockResolvedValue({ container: {} }),
+    },
+  })),
+}));
+const CosmosClient = require('@azure/cosmos').CosmosClient;
+
+jest.mock('./execute-migration', () => ({
+  executeMigration: jest.fn(),
+}));
+const { executeMigration } = require('./execute-migration');
+
+const { migrate } = require('./action');
+
+describe('migrate', () => {
+  beforeEach(() => {
+    // Mock the necessary dependencies and environment variables
+    jest.resetModules();
+  });
+
+  it('should execute migrations successfully', async () => {
+    process.env.DB_URL = 'mock-db-url';
+    process.env.DB_KEY = 'mock-db-key';
+    process.env.MIGRATIONS_DIR = 'mock-migrations-dir';
+    jest.mock('path', () => ({
+      resolve: jest.fn().mockReturnValue('mock-migrations-dir'),
+      basename: jest.fn().mockReturnValue('migration1.js'),
+    }));
+
+    const mockClientInstance = {};
+    CosmosClient.mockImplementation(() => mockClientInstance);
+
+    // Call the migrate function
+    await migrate({});
+
+    // Assertions
+    expect(CosmosClient).toHaveBeenCalledWith({
+      endpoint: 'mock-db-url',
+      key: 'mock-db-key',
+    });
+    expect(executeMigration).toHaveBeenCalledTimes(2);
+
+    expect(executeMigration).toHaveBeenNthCalledWith(
+      1,
+      mockClientInstance,
+      expect.stringContaining('mock-migrations-dir/migration1.js')
+    );
+    expect(executeMigration).toHaveBeenCalledWith(
+      mockClientInstance,
+      expect.stringContaining('mock-migrations-dir/migration2.js')
+    );
+  });
+
+  it('should handle missing environment variables', async () => {
+    // Mock the necessary functions and objects
+    const mockConsoleError = jest.spyOn(console, 'error').mockImplementation();
+
+    fileExistsSync.mockReturnValue(false);
+
+    process.exit = jest.fn();
+
+    process.env.DB_URL = '';
+    process.env.DB_KEY = '';
+    process.env.MIGRATIONS_DIR = '';
+
+    // Call the migrate function
+    await migrate({});
+
+    // Assertions
+    expect(mockConsoleError).toHaveBeenCalledTimes(4);
+    expect(mockConsoleError).toHaveBeenNthCalledWith(1, 'Errors:');
+    expect(mockConsoleError).toHaveBeenNthCalledWith(
+      2,
+      'Environment var DB_URL or --dbURL option must be provided!'
+    );
+    expect(mockConsoleError).toHaveBeenNthCalledWith(
+      3,
+      'Environment var DB_KEY or --dbKey option must be provided!'
+    );
+    expect(mockConsoleError).toHaveBeenNthCalledWith(
+      4,
+      'Environment var MIGRATIONS_DIR or --migrationsDir option must be provided and the folder must exist!'
+    );
+    expect(process.exit).toHaveBeenCalledWith(1);
+  });
+});
