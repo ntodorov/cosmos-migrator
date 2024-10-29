cc096f12d700601d248eaa5d1e169b16b14bdf84
Nik Todorov <ntodorov@gmail.com>
2024-10-29 13:50:53 -0600

    breaking change mitiagated for now. --useDotEnv is added but marked as depricated.

--- src/index.js
+++ src/index.js
@@ -26,6 +26,10 @@ program
     '-md, --migrationsDir <full folder name>',
     'Folder containing the migration scripts. Can also be set in the environment variable MIGRATIONS_DIR'
   )
+  .option(
+    '-ude, --useDotEnv',
+    'DEPRICATED! Now the cli will always check for environment variables in a .env file. '
+  )
   .action(migrate);
 
 program.parse();
